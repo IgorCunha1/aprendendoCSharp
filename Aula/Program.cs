14cf2bcf67bc22361a61eff855c5f5315512b24f
IGOR IVSON DA SILVA CUNHA <igorivson@outlook.com>
2021-10-18 17:21:21 -0300

    correcao

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -6,15 +6,13 @@ namespace Aula
     {
         static void Main(string[] args)
         {
-             ContaCorrente conta = new ContaCorrente();
+            ContaCorrente conta = new ContaCorrente();
 
             conta.setTitular("Igor");
             conta.setSaldo(100.0);
 
             Console.WriteLine(conta.getTitular());
-
             conta.Sacar(10);
-
             Console.WriteLine(conta.getSaldo());
 
         }
