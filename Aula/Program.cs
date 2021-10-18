dde54bc574bfb4d01b5f5b8aa59749a33c15c6e3
IGOR IVSON DA SILVA CUNHA <igorivson@outlook.com>
2021-10-18 16:39:34 -0300

    novo hotfix

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -7,6 +7,7 @@ namespace Aula
         static void Main(string[] args)
         {
             Console.WriteLine("Aqui está uma nova feature!");
+            Console.WriteLine("Aqui está uma nova 2222!");
         }
     }
 }
