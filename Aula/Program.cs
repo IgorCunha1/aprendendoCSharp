65ff08e66cc65120d513067f1bfed5d1760205f9
IGOR IVSON DA SILVA CUNHA <igorivson@outlook.com>
2021-10-18 17:13:16 -0300

    criacao de metodo sacar + encapsulando atributos

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -6,7 +6,17 @@ namespace Aula
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aqui está uma nova feature!");
+             ContaCorrente conta = new ContaCorrente();
+
+            conta.setTitular("Igor");
+            conta.setSaldo(100.0);
+
+            Console.WriteLine(conta.getTitular());
+
+            conta.Sacar(10);
+
+            Console.WriteLine(conta.getSaldo());
+
         }
     }
 }
