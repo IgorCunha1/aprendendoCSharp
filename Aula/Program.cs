4aaeed677dea76ed27e3c99fc58e68dae7a9e7f7
IGOR IVSON DA SILVA CUNHA <igorivson@outlook.com>
2021-10-18 21:37:44 -0300

    novo commit

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -9,8 +9,9 @@ namespace Aula
 
             ContaCorrente conta = new ContaCorrente("igor", 112, 123, 123.0);
             ContaCorrente contaDois = new ContaCorrente("Joao", 112, 123, 123.99);
+            ContaCorrente contaTres = new ContaCorrente("Maria", 112, 123, 123.99);
 
-            ContaCorrente[] cc = { conta, contaDois };
+            ContaCorrente[] cc = { conta, contaDois, contaTres };
 
             foreach(ContaCorrente contas in cc)
             {
